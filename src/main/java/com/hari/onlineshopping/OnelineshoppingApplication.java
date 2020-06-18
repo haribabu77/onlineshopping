04f5647ffab290c47797ffa2187367bd241d3c42
HariBabu <hguraka@gmail.com>
2020-06-18 13:14:27 -0500

    onlineshopping first commit

---
+++ src/main/java/com/hari/onlineshopping/OnelineshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.hari.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelineshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnelineshoppingApplication.class, args);
+	}
+
+}
